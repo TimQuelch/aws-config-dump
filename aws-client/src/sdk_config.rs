795e6293de33a083316db4ed451f1a219d98901b
Tim Quelch <tim@tquelch.com>
2026-05-01 19:17:15 +1000

    Make retries slightly less aggressive

--- aws-client/src/sdk_config.rs
+++ aws-client/src/sdk_config.rs
@@ -4,8 +4,8 @@ use aws_config::{SdkConfig, retry::RetryConfig};
 
 pub async fn load_config(profile: Option<String>) -> SdkConfig {
     let loader = aws_config::from_env().retry_config(
-        RetryConfig::standard()
-            .with_initial_backoff(Duration::from_millis(50))
+        RetryConfig::adaptive()
+            .with_initial_backoff(Duration::from_millis(100))
             .with_max_backoff(Duration::from_secs(60))
             .with_max_attempts(100),
     );
